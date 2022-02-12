8abdd6d53a4b572965f8684014e54f0e2f73498b
Dan Stoian <danmihaistoian@gmail.com>
2022-02-12 17:52:17 +0200

    Initial Commit

---
+++ SweetIRC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SweetIRC
+//
+//  Created by Dan Stoian on 12.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SweetIRC/SweetIRCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetIRCApp.swift
+//  SweetIRC
+//
+//  Created by Dan Stoian on 12.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SweetIRCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
